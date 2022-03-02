ded95eb9bda0b27b325202ed553d54494254a1e0
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-01 20:33:56 -0500

    API call

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import './App.css';
 
 function App() {
  
+  const [recipesFound, setRecipesFound] = useState([]);
+  const [recipeSearch, setrecipeSearch] = useState('');
+
+  const searchForRecipes = async(query:string): Promise<any> => {
+    const result = await fetch(`http://localhost:3001/?search=${query}`)
+    return (await result.json()).results;
+  }
+
+  useEffect(() => {
+    (async () => {
+      const query = encodeURIComponent(recipeSearch)
+      const response = await searchForRecipes(query);
+      setRecipesFound(response)
+    })()
+
+  })
   
   return (
     <div className="App">
