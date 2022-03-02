0fdd8205acc7394fcbe1217afbcdada09083ba01
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-01 21:23:24 -0500

    Search input

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,41 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
- 
-  const [recipesFound, setRecipesFound] = useState([]);
-  const [recipeSearch, setrecipeSearch] = useState('');
 
-  const searchForRecipes = async(query:string): Promise<any> => {
+  const [recipesFound, setRecipesFound] = useState([]);
+  const [recipeSearch, setRecipeSearch] = useState('');
+
+  const searchForRecipes = async (query: string): Promise<any> => {
     const result = await fetch(`http://localhost:3001/?search=${query}`)
     return (await result.json()).results;
   }
 
   useEffect(() => {
     (async () => {
-      const query = encodeURIComponent(recipeSearch)
-      const response = await searchForRecipes(query);
-      setRecipesFound(response)
-    })()
 
-  })
-  
+      const query = encodeURIComponent(recipeSearch)
+      if (query) {
+        const response = await searchForRecipes(query);
+        setRecipesFound(response)
+      }
+    })()
+  }, [recipeSearch])
+
+  const search = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const form = event.target as HTMLFormElement
+    const input = form.querySelector('#searchText') as HTMLInputElement;
+    setRecipeSearch(input.value)
+
+  }
+
   return (
     <div className="App">
-      
+      <h1>Recipe Search App</h1>
+      <form className='searchForm' onSubmit={event => search(event)}>
+        <input id='searchText' type="text" />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
